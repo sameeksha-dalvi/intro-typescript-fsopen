d51e28edc923350aa9aa54a61a62bfb97a4abcc9
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-07-18 17:56:54 +0530

    add calculator function with type Operation and Results for return values

---
+++ calculator.ts
@@ -0,0 +1,19 @@
+type Operation = 'multiply' | 'add' | 'divide';
+type Result = string | number | undefined;
+
+
+const calculator = (a: number, b: number, op: Operation) : Result => {
+    if (op === 'multiply') {
+        return a * b;
+    } else if (op === 'add') {
+        return a + b;
+    } else if (op === 'divide') {
+        if( b === 0){
+            return 'can\'t divide by 0!'
+        }
+        return a / b;
+    }
+}
+
+const ans = calculator(2,4,'add')
+console.log('Ans: ',ans)
