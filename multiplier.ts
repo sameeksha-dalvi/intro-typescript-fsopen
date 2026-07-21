b0ccbebce4f3c16608d012567e91982d77c4430d
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-07-21 16:51:57 +0530

    get multiplier to work with command line parameters

--- multiplier.ts
+++ multiplier.ts
@@ -3,3 +3,8 @@ const multiplicator = (a: number, b: number, printText: string) => {
 }
 
 multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+
+const a: number = Number(process.argv[2])
+const b: number = Number(process.argv[3])
+
+multiplicator(a,b, `Multiplied ${a} and ${b}, the result is: `);
