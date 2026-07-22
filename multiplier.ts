5b73f12ec57bebdd6946b8690fccf5749838d34a
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2026-07-22 16:59:53 +0530

    refactor multiplier code with improvised version

--- multiplier.ts
+++ multiplier.ts
@@ -1,10 +1,43 @@
+interface MultiplyValues {
+    value1: number;
+    value2: number;
+}
+
+
+const parseArguments = (args: string[]): MultiplyValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+        return {
+            value1: Number(args[2]),
+            value2: Number(args[3])
+        }
+    }else{
+        throw new Error('Provided values are not numbers!')
+    }
+}
+
+
 const multiplicator = (a: number, b: number, printText: string) => {
     console.log(printText, a * b)
 }
 
-multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
+// multiplicator(2, 4, 'Multiplied numbers 2 and 4, the result is:');
 
-const a: number = Number(process.argv[2])
-const b: number = Number(process.argv[3])
+// const a: number = Number(process.argv[2])
+// const b: number = Number(process.argv[3])
 
-multiplicator(a,b, `Multiplied ${a} and ${b}, the result is: `);
+// multiplicator(a, b, `Multiplied ${a} and ${b}, the result is: `);
+
+try {
+    const{value1, value2} = parseArguments(process.argv);
+    multiplicator(value1, value2, `Multiplied ${value1} and ${value2}, the result is: `);
+
+} catch (error: unknown) {
+    let errorMsg = 'Something bad happened.'
+    if(error instanceof Error){
+        errorMsg += ' Error: '+ error.message;
+    }
+    console.log(errorMsg)
+}
